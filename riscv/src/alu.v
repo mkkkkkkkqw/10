96b972ac1f7e1b7f254242436add5b2ebbb94a32
mkkkkkkkqw <156543266+mkkkkkkkqw@users.noreply.github.com>
2024-01-15 06:37:30 +0800

    Update alu.v

--- riscv/src/alu.v
+++ riscv/src/alu.v
@@ -36,61 +36,61 @@ wire [31:0]arith_op1=val1;
 wire [31:0]arith_op2=(opcode==7'b0110011)?val2:imm;//判断是否是计算
 reg [31:0] arith_res;
 always @(*)begin
-    // case(funct3)
-    //     3'b000:begin//add
-    //         if(funct7==0)begin
-    //             arith_res=arith_op1+arith_op2;
-    //         end
-    //         else begin
-    //             arith_res=arith_op1-arith_op2;
-    //         end
-    //         // arith_res=arith_op1+arith_op2;
-    //     end
-    //     3'b001:begin//sll
-    //         arith_res=arith_op1<<arith_op2;
-    //     end
-    //     3'b010:begin//slt
-    //         arith_res=(arith_op1<arith_op2)?1:0;
-    //     end
-    //     3'b011:begin//sltu
-    //         arith_res=(arith_op1<arith_op2)?1:0;
-    //     end
-    //     3'b100:begin//xor
-    //         arith_res=arith_op1^arith_op2;
-    //     end
-    //     3'b101:begin//srl or sra
-    //         if(funct7==0)begin  //逻辑右移
-    //             arith_res=arith_op1>>arith_op2[5:0];
-    //         end
-    //         else begin
-    //             arith_res=arith_op1>>>arith_op2[5:0];//算术右移
-    //         end
-    //         // arith_res=arith_op1>>arith_op2[4:0];
-    //     end
-    //     3'b110:begin//or
-    //         arith_res=arith_op1|arith_op2;
-    //     end
-    //     3'b111:begin//and
-    //         arith_res=arith_op1&arith_op2;
-    //     end
-    //     default:begin
-    //         arith_res=0;
-    //     end
-    // endcase
-        case (funct3)
-      `FUNCT3_ADD:  // ADD or SUB
-      if (opcode == `OPCODE_ARITH && funct7)   arith_res = arith_op1 - arith_op2;
-      else          arith_res = arith_op1 + arith_op2;
-      `FUNCT3_XOR:  arith_res = arith_op1 ^ arith_op2;
-      `FUNCT3_OR:   arith_res = arith_op1 | arith_op2;
-      `FUNCT3_AND:  arith_res = arith_op1 & arith_op2;
-      `FUNCT3_SLL:  arith_res = arith_op1 << arith_op2;
-      `FUNCT3_SRL:  // SRL or SRA
-      if (funct7)   arith_res = $signed(arith_op1) >> arith_op2[5:0];
-      else          arith_res = arith_op1 >> arith_op2[5:0];
-      `FUNCT3_SLT:  arith_res = ($signed(arith_op1) < $signed(arith_op2));
-      `FUNCT3_SLTU: arith_res = (arith_op1 < arith_op2);
+    case(funct3)
+        3'b000:begin//add
+            if(funct7==0)begin
+                arith_res=arith_op1+arith_op2;
+            end
+            else begin
+                arith_res=arith_op1-arith_op2;
+            end
+            // arith_res=arith_op1+arith_op2;
+        end
+        3'b001:begin//sll
+            arith_res=arith_op1<<arith_op2;
+        end
+        3'b010:begin//slt
+            arith_res=(arith_op1<arith_op2)?1:0;
+        end
+        3'b011:begin//sltu
+            arith_res=(arith_op1<arith_op2)?1:0;
+        end
+        3'b100:begin//xor
+            arith_res=arith_op1^arith_op2;
+        end
+        3'b101:begin//srl or sra
+            if(funct7==0)begin  //逻辑右移
+                arith_res=arith_op1>>arith_op2[5:0];
+            end
+            else begin
+                arith_res=arith_op1>>>arith_op2[5:0];//算术右移
+            end
+            // arith_res=arith_op1>>arith_op2[4:0];
+        end
+        3'b110:begin//or
+            arith_res=arith_op1|arith_op2;
+        end
+        3'b111:begin//and
+            arith_res=arith_op1&arith_op2;
+        end
+        default:begin
+            arith_res=0;
+        end
     endcase
+    //     case (funct3)
+    //   `FUNCT3_ADD:  // ADD or SUB
+    //   if (opcode == `OPCODE_ARITH && funct7)   arith_res = arith_op1 - arith_op2;
+    //   else          arith_res = arith_op1 + arith_op2;
+    //   `FUNCT3_XOR:  arith_res = arith_op1 ^ arith_op2;
+    //   `FUNCT3_OR:   arith_res = arith_op1 | arith_op2;
+    //   `FUNCT3_AND:  arith_res = arith_op1 & arith_op2;
+    //   `FUNCT3_SLL:  arith_res = arith_op1 << arith_op2;
+    //   `FUNCT3_SRL:  // SRL or SRA
+    //   if (funct7)   arith_res = $signed(arith_op1) >> arith_op2[5:0];
+    //   else          arith_res = arith_op1 >> arith_op2[5:0];
+    //   `FUNCT3_SLT:  arith_res = ($signed(arith_op1) < $signed(arith_op2));
+    //   `FUNCT3_SLTU: arith_res = (arith_op1 < arith_op2);
+    // endcase
 end
 
 
